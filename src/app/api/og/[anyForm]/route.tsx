42b075125856bb1a40cbef6251cf42b84c01d3d2
Juyeong Maing <mjy9088@naver.com>
2025-01-19 23:12:47 +0900

    fix og bug

--- src/app/api/og/[anyForm]/route.tsx
+++ src/app/api/og/[anyForm]/route.tsx
@@ -1,11 +1,7 @@
-import { contrastColor } from "@/_internal/lib/contrastColor";
 import { encode } from "@/_internal/lib/encode";
-import { moreInfo } from "@/_internal/lib/moreInfo";
-import { ImageResponse } from "next/og";
+import { og } from "@/_internal/pages/api/og";
 import { NextRequest } from "next/server";
-import { defaultRenderOptions } from "../../../../_internal/lib/defaultRenderOptions";
 import { isValidName } from "../../../../_internal/lib/isValidName";
-import { renderToSvg } from "../../../../_internal/lib/renderToSvg";
 
 export async function GET(
   _request: NextRequest,
@@ -16,91 +12,5 @@ export async function GET(
   if (!polyomino || encode(polyomino) !== anyForm) {
     return new Response("Not Found", { status: 404 });
   }
-  const [symmetryGroup] = moreInfo([polyomino, anyForm]);
-
-  const { backgroundColor, fillColorHexCode, strokeColorHexCode } =
-    defaultRenderOptions([polyomino, anyForm]);
-
-  const svg = renderToSvg(polyomino, {
-    fillColorHexCode,
-    strokeColorHexCode,
-    contain: [630, 630],
-    backgroundColor: "transparent",
-  });
-
-  const badgeStyle = {
-    padding: 8,
-    borderRadius: 12,
-    background: `#${fillColorHexCode}`,
-    color: `#${contrastColor(fillColorHexCode)}`,
-  };
-
-  return new ImageResponse(
-    (
-      <div
-        style={{
-          width: 1200,
-          height: 630,
-          display: "flex",
-          alignItems: "center",
-          backgroundColor,
-        }}
-      >
-        <img
-          src={`data:image/svg+xml;base64,${btoa(svg)}`}
-          width={630}
-          height={630}
-        />
-        <div
-          style={{
-            height: "100%",
-            flex: 1,
-            background: `linear-gradient(to right, #FFF0, white 50%, white)`,
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-            fontSize: 48,
-            wordBreak: "break-all",
-          }}
-        >
-          Polyomino
-          <div style={{ padding: 24 }}>{anyForm}</div>
-          <div
-            style={{
-              display: "flex",
-              gap: 12,
-              fontSize: 16,
-              flexWrap: "wrap",
-              justifyContent: "center",
-            }}
-          >
-            <span style={badgeStyle}>Symmetry group: {symmetryGroup}</span>
-            <span style={badgeStyle}>Tile count: {polyomino.length}</span>
-            {[
-              "All",
-              "Rotation2FoldMirror90",
-              "Rotation2FoldMirror45",
-              "Rotation2Fold",
-            ].includes(symmetryGroup) && (
-              <span style={badgeStyle}>Rotation 2 Fold</span>
-            )}
-            {["All", "Rotation4Fold"].includes(symmetryGroup) && (
-              <span style={badgeStyle}>Rotation 4 Fold</span>
-            )}
-            {["All", "Rotation2FoldMirror90", "Mirror90"].includes(
-              symmetryGroup
-            ) && <span style={badgeStyle}>Mirror 90°</span>}
-            {["All", "Rotation2FoldMirror45", "Mirror45"].includes(
-              symmetryGroup
-            ) && <span style={badgeStyle}>Mirror 45°</span>}
-          </div>
-        </div>
-      </div>
-    ),
-    {
-      width: 1200,
-      height: 630,
-    }
-  );
+  return og([polyomino, anyForm]);
 }
